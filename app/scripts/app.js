fc8b10a7a3c3f13cfd48045bbf9118c53934e7ac
Bridgette <bridgettekincaid@gmail.com>
2015-01-18 21:31:14 -0600

    todolist

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,70 +1,31 @@
-function updateItemStatus() {
-    var cbId = this.id.replace("cb_", "");
-    var itemText = document.getElementById("item_" + cbId);
+var app = angular.module('app', []);
+
+app.controller('TodoCtrl', [function($scope, $filter){  
+ 
+    var todos = [];
+    var newtodo = '';
+
+    $scope.$watch('todos', function(){
+        $scope.remaining = $filter($scope, todos, {completed:false}).length;
+        $scope.allchecked = !$scope.remaining;
+    }, true);
     
-    if (this.checked) {
-    itemText.className = "checked";
-
-} else {
-    itemText.className = "";
-}
-
-}
-
-function renameItem() {
-    var newText = prompt("What should this item be renamed to?");
-   if (!newText || newText == "" || newText == " ") {
-       return false;
-   }
-     
-    this.innertext = newText;
-}
-
-function removeItem() {
-    this.style.display = "none";
-}
-function addNewItem(list, itemText) {
-
-    var date = new Date();
-    var id = "" + date.getHours() + date.getMinutes() + date.getSeconds() + date.getMilliseconds();
+    $scope.removeTodo = function(index){
+        $scope.todos.splice(index,1);
+    };
     
-    var listItem = document.createElement("li");
-    listItem.id = "li_" + id;
-    listItem.ondblclick = removeItem;
-    
-    var checkBox = document.createElement("input");
-    checkBox.type = "checkbox";
-    checkBox.id = "cb_" + id;
-    checkBox.onclick = updateItemStatus;
-    
-    var span = document.createElement("span");
-    span.id = "item_" + id;
-    span.innerText = itemText;
-    span.onclick = renameItem;
-    span.ondblclick = removeItem;
-    
-    listItem.appendChild(checkBox);
-    listItem.appendChild(span);
-
-    list.appendChild(listItem);
-
-    } 
-var inItemText = document.getElementById("inItemText");
-inItemText.focus();
-inItemText.onkeyup = function(event) {
-  
-        //Event.which (13 -> ENTER
-        //Only proceed if key press is ENTER key
-     if (event.which == 13) {
-        var itemText = inItemText.value;
-         
-    if(itemText == "" || itemText == " ") {
-        return false;
+    $scope.addTodo = function(){
+        $scope.todos.push({
+            name: $scope.newtodo, 
+            completed: false
+        });
     }
-    addNewItem(document.getElementById("todoList"), itemText); 
+    
+    $scope.checkAllTodo = function(allchecked){
+        $scope.todos.forEach(function(todo){
+            todo.completed = allchecked;
+        })
+    }     
+}]);
 
-         inItemText.focus();
-         inItemText.select();
-};
-}
-
+// todos: newTodo: name, completed
---
+++ app/scripts/todos.php
@@ -0,0 +1,6 @@
+[
+{"name": "Tutorial for Angular", "completed":false},
+{"name": "Tutorial for Angular", "completed":false},
+{"name": "Style", "completed":false},
+{"name": "Comment", "completed":true}
+]
