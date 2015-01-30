74a7b8b6e1b73cbaaee4a72b1c68e19f961c0465
Alissa <alissalikavec@gmail.com>
2015-01-30 06:51:29 +0000

    Angular changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,23 +1,47 @@
-function TodoCtrl($scope) {
-  
-  $scope.todos = [
-    {text:'Learn AngularJS', done:false},         
-    {text: 'Build an app', done:false}
-  ];
-  
-  $scope.getTotalTodos = function () {
-    return $scope.todos.length;
+var app = angular.module('todo', []);
+
+app.directive('ngBlur', function(){
+  return function(scope, elem, attrs){
+    elem.bind('blur', function(){
+      scope.$apply(attrs.ngBlur);
+    })
+  }
+})
+app.controller('TodoCtrl', ['$scope', '$interval', function($scope, $interval){
+  $scope.todos = [];
+  $scope.expiredTodos = [];
+
+$scope.expireTodo = function(todo){
+    console.log(todo);
+    $scope.expiredTodos.push(todo);
+    $scope.todos.splice(todo,1);
+    console.log("We finished expireTodo! Here's what's expired: " + $scope.expiredTodos);
   };
-  
-  
-  $scope.addTodo = function () {
-    $scope.todos.push({text:$scope.formTodoText, done:false});
-    $scope.formTodoText = '';
+
+  $scope.addTodo = function(){
+    $scope.todos.push({
+      name : $scope.newtodo,
+      completed : false
+    });
+    $scope.newtodo = '';
+    return false;
   };
-  
-    $scope.clearCompleted = function () {
-        $scope.todos = _.filter($scope.todos, function(todo){
-            return !todo.done;
-        });
-    };
-}
+
+  $scope.editTodo = function(todo){
+    todo.editing = false;
+  };
+
+  $scope.checkAllTodo = function(){
+    $scope.todos.forEach(function(todo){
+      if (todo.completed) {
+        console.log(todo);
+        console.log("Removing " + todo.name);
+        $scope.expireTodo(todo);
+      }
+      console.log("Checking!");
+    })
+  };
+
+ $interval(function(){ $scope.checkAllTodo(); }, 3000);
+
+}]);
