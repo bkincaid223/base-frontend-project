84b911469730d4b80f12f187b9f015337c661868
Bridgette <bridgettekincaid@gmail.com>
2015-02-12 23:17:55 -0600

    Todo Changes

--- app/scripts/todos.php
+++
@@ -1,6 +0,0 @@
-[
-{"name": "Tutorial for Angular", "completed":false},
-{"name": "Tutorial for Angular", "completed":false},
-{"name": "Style", "completed":false},
-{"name": "Comment", "completed":true}
-]
